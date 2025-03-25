895fd416ddf67b0398f53f33cf3bed824b75dbf5
Anmol Kamoji <anmolkamoji2@gmail.com>
2025-03-25 13:28:41 +0530

    intellij

---
+++ FileHandling.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.io.IOException;
+@SuppressWarnings("ALL")
+public class FileHandling {
+    public static void main(String[] args) {
+        File myFile = new File("C:\\coding\\java-practice\\Java-practice\\newFile.txt");
+        try {
+            myFile.createNewFile();
+        } catch (IOException ex) {
+            System.out.println("Unable to create this file");
+            ex.printStackTrace();
+        }
+    }
+}
--- LinkedLists.java
+++ LinkedLists.java
@@ -1,6 +1,5 @@
-import java.util.*;
-
 public class LinkedLists {
+    // Inner Node class
     class Node {
         String data;
         Node next;
@@ -11,18 +10,26 @@ public class LinkedLists {
         }
     }
 
-    // add -first, last
+    // Instance variable for the head of the list
+    private Node head;
+
+    // Constructor to initialize an empty list
+    public LinkedLists() {
+        this.head = null;
+    }
+
+    // Add a node at the beginning of the list
     public void addFirst(String data) {
         Node newNode = new Node(data);
         if (head == null) {
             head = newNode;
             return;
         }
-
         newNode.next = head;
         head = newNode;
     }
 
+    // Add a node at the end of the list
     public void addLast(String data) {
         Node newNode = new Node(data);
         if (head == null) {
@@ -31,11 +38,33 @@ public class LinkedLists {
         }
 
         Node currNode = head;
+        while (currNode.next != null) {
+            currNode = currNode.next;
+        }
+        currNode.next = newNode;
     }
 
+    // Print the linked list
+    public void printList() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+
+        Node currNode = head;
+        while (currNode != null) {
+            System.out.print(currNode.data + " -> ");
+            currNode = currNode.next;
+        }
+        System.out.println("null");
+    }
+
+    // Main method to test the linked list
     public static void main(String[] args) {
         LinkedLists list = new LinkedLists();
         list.addFirst("a");
         list.addFirst("is");
+        list.addLast("list");
+        list.printList();
     }
 }
