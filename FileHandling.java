2cad3d67ea46a74d900d74860f479b2fd580bfed
Anmol Kamoji <anmolkamoji2@gmail.com>
2025-03-25 14:45:34 +0530

    Update FileHandling.java

--- FileHandling.java
+++ FileHandling.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 @SuppressWarnings("ALL")
 public class FileHandling {
     public static void main(String[] args) {
+        // code to create a new file
         File myFile = new File("C:\\coding\\java-practice\\Java-practice\\newFile.txt");
         try {
             myFile.createNewFile();
